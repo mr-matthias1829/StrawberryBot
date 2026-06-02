ab0c4bcf423e39989ce76ec604b686befd82d4e0
mr-matthias1829 <180924703+mr-matthias1829@users.noreply.github.com>
2026-06-02 11:00:55 +0200

    maybe sped up the camera...? attempt 5

--- Python/Main.py
+++ Python/Main.py
@@ -216,7 +216,7 @@ def run_webcam() -> None:
         while True:
             t_read = time.perf_counter()
 
-            for _ in range(60):
+            for _ in range(1):
                 cap.grab()
 
             ok, frame = cap.retrieve()
